c4874d99e92a3d4fba22daed454189d9969a710a
Kevin Jones <kevin@vcsjones.com>
2016-06-07 11:17:08 -0400

    Added check that primary signature should be RSA / DSA.

---
+++ AuthenticodeLint/Rules/10012-RsaDsaPrimarySignatureRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AuthenticodeLint.Rules
+{
+    public class RsaDsaPrimarySignatureRule : IAuthenticodeSignatureRule
+    {
+        public int RuleId { get; } = 10012;
+
+        public string RuleName { get; } = "RSA/DSA Primary Signature";
+
+        public string ShortDescription { get; } = "Primary signature should be RSA or DSA.";
+
+        public RuleResult Validate(Graph<Signature> graph, SignatureLogger verboseWriter, CheckConfiguration configuration)
+        {
+            var primary = graph.SingleOrDefault()?.Node;
+            //There are zero signatures.
+            if (primary == null)
+            {
+                return RuleResult.Fail;
+            }
+            var info = BitStrengthCalculator.CalculateStrength(primary.SignerInfo.Certificate);
+            if (info.AlgorithmName != PublicKeyAlgorithm.RSA && info.AlgorithmName != PublicKeyAlgorithm.DSA)
+            {
+                verboseWriter.LogSignatureMessage(primary.SignerInfo, $"Primary signature should use RSA or DSA key but uses ${info.AlgorithmName.ToString()}");
+                return RuleResult.Fail;
+            }
+            return RuleResult.Pass;
+        }
+    }
+}
