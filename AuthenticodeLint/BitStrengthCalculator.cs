8e4e62cff2c44ae1458a247693911e7efc37e35c
Kevin Jones <kevin@vcsjones.com>
2016-06-07 11:05:02 -0400

    Added check for minimum key size in signature certificates.

---
+++ AuthenticodeLint/BitStrengthCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AuthenticodeLint
+{
+    public static class BitStrengthCalculator
+    {
+        public static CertificateBitStrength CalculateStrength(X509Certificate2 certificate)
+        {
+            PublicKeyAlgorithm keyAlgorithm;
+            int? bitSize;
+            switch (certificate.PublicKey.Oid.Value)
+            {
+                case KnownOids.X509Algorithms.Ecc:
+                    var parameterOid = OidParser.ReadFromBytes(certificate.PublicKey.EncodedParameters.RawData);
+                    switch (parameterOid.Value)
+                    {
+                        case KnownOids.EccCurves.EcdsaP256:
+                            keyAlgorithm = PublicKeyAlgorithm.ECDSA;
+                            bitSize = 256;
+                            break;
+                        case KnownOids.EccCurves.EcdsaP384:
+                            keyAlgorithm = PublicKeyAlgorithm.ECDSA;
+                            bitSize = 384;
+                            break;
+                        case KnownOids.EccCurves.EcdsaP521:
+                            keyAlgorithm = PublicKeyAlgorithm.ECDSA;
+                            bitSize = 521;
+                            break;
+                        default:
+                            keyAlgorithm = PublicKeyAlgorithm.Other;
+                            bitSize = null;
+                            break;
+                    }
+                    break;
+                case KnownOids.X509Algorithms.RSA:
+                    keyAlgorithm = PublicKeyAlgorithm.RSA;
+                    bitSize = certificate.PublicKey.Key.KeySize;
+                    break;
+                default:
+                    keyAlgorithm = PublicKeyAlgorithm.Other;
+                    bitSize = null;
+                    break;
+            }
+            return new CertificateBitStrength(keyAlgorithm, bitSize);
+        }
+    }
+
+    public class CertificateBitStrength
+    {
+        public CertificateBitStrength(PublicKeyAlgorithm algorithmName, int? bitSize)
+        {
+            AlgorithmName = algorithmName;
+            BitSize = bitSize;
+        }
+
+        public PublicKeyAlgorithm AlgorithmName { get; }
+        public int? BitSize { get; }
+    }
+
+    public enum PublicKeyAlgorithm
+    {
+        Other = 0,
+        RSA = 1,
+        DSA = 2,
+        ECDSA = 3
+    }
+}
---
+++ AuthenticodeLint/Rules/10011-StrongKeyLengthRule.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace AuthenticodeLint.Rules
+{
+    public class StrongKeyLengthRule : IAuthenticodeSignatureRule
+    {
+        private const int MIN_RSADSA_KEY_SIZE = 2048;
+
+        public int RuleId { get; } = 10011;
+
+        public string RuleName { get; } = "Strong Key Length";
+
+        public string ShortDescription { get; } = "Validates the key length of a signing certificate.";
+
+        public RuleResult Validate(Graph<Signature> graph, SignatureLogger verboseWriter, CheckConfiguration configuration)
+        {
+            var signatures = graph.VisitAll();
+            var result = RuleResult.Pass;
+            foreach (var signature in signatures)
+            {
+                var keyInfo = BitStrengthCalculator.CalculateStrength(signature.SignerInfo.Certificate);
+                switch (keyInfo.AlgorithmName)
+                {
+                    case PublicKeyAlgorithm.ECDSA:
+                        if (keyInfo.BitSize == null)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, "Signature uses ECDSA with an unknown curve.");
+                            result = RuleResult.Fail;
+                        }
+                        //We don't actually check the key size for ECDSA since all known values are acceptable.
+                        break;
+                    case PublicKeyAlgorithm.RSA:
+                        if (keyInfo.BitSize == null)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, "Signature has an unknown RSA key size.");
+                            result = RuleResult.Fail;
+                        }
+                        else if (keyInfo.BitSize < MIN_RSADSA_KEY_SIZE)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses a RSA key of size {keyInfo.BitSize} which is below the recommended {MIN_RSADSA_KEY_SIZE}.");
+                            result = RuleResult.Fail;
+                        }
+                        break;
+                    case PublicKeyAlgorithm.DSA:
+                        if (keyInfo.BitSize == null)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, "Signature has an unknown DSA key size.");
+                            result = RuleResult.Fail;
+                        }
+                        else if (keyInfo.BitSize < MIN_RSADSA_KEY_SIZE)
+                        {
+                            //Effectively, 1024 is the max for a DSA key, so this will likely always fail.
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses a DSA key of size {keyInfo.BitSize} which is below the recommended {MIN_RSADSA_KEY_SIZE}.");
+                            result = RuleResult.Fail;
+                        }
+                        break;
+                    case PublicKeyAlgorithm.Other:
+                        goto default;
+                    default:
+                        verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses an unknown algorithm.");
+                        result = RuleResult.Fail;
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
