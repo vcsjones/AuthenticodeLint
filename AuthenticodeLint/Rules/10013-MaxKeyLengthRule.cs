69bffd2bea6076b18ce7949f83a711d60e2e15db
Kevin Jones <kevin@vcsjones.com>
2016-06-07 11:44:47 -0400

    Added check for maximum key lengths.
    Rationale: not all clients support huge key sizes.
    
    RSA is limited to 4096-bit as there is little reason to use a key
    size greater than 4096.
    DSA is limited to 1024 as most clients don't support anything different. This effectively means
    that check #10013 and #10011 are at odds with one another with DSA. This is an acceptable situation
    as the use of DSA is fairly rare and RSA is preferred over it. Ultimately, if DSA is being used, you
    need to make a choice of whether client compatibility or key size is important, or switch to RSA.
    ECDSA is limited to P-256 or P-384. This excludes P-521. It's not immediately clear if all clients
    support P-521 though it appears 384 is.

---
+++ AuthenticodeLint/Rules/10013-MaxKeyLengthRule.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace AuthenticodeLint.Rules
+{
+    public class MaxKeyLengthRule : IAuthenticodeSignatureRule
+    {
+        private const int MAX_ECDSA_KEY_SIZE = 384;
+        private const int MAX_RSA_KEY_SIZE = 4096;
+        private const int MAX_DSA_KEY_SIZE = 1024;
+
+        public int RuleId { get; } = 10013;
+
+        public string RuleName { get; } = "Maximum Key Length";
+
+        public string ShortDescription { get; } = "Validates the maximum key length of a signing certificate.";
+
+        public RuleResult Validate(Graph<Signature> graph, SignatureLogger verboseWriter, CheckConfiguration configuration)
+        {
+            var signatures = graph.VisitAll();
+            var result = RuleResult.Pass;
+            foreach (var signature in signatures)
+            {
+                var keyInfo = BitStrengthCalculator.CalculateStrength(signature.SignerInfo.Certificate);
+                switch (keyInfo.AlgorithmName)
+                {
+                    case PublicKeyAlgorithm.ECDSA:
+                        if (keyInfo.BitSize == null)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, "Signature uses ECDSA with an unknown curve.");
+                            result = RuleResult.Fail;
+                        }
+                        else if (keyInfo.BitSize > MAX_ECDSA_KEY_SIZE)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses ECDSA signature with a key size of {keyInfo.BitSize} exeeding maximum size of {MAX_ECDSA_KEY_SIZE}.");
+                            result = RuleResult.Fail;
+                        }
+                        break;
+                    case PublicKeyAlgorithm.RSA:
+                        if (keyInfo.BitSize == null)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, "Signature has an unknown RSA key size.");
+                            result = RuleResult.Fail;
+                        }
+                        else if (keyInfo.BitSize > MAX_RSA_KEY_SIZE)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses RSA signature with a key size of {keyInfo.BitSize} exeeding maximum size of {MAX_RSA_KEY_SIZE}.");
+                            result = RuleResult.Fail;
+                        }
+                        break;
+                    case PublicKeyAlgorithm.DSA:
+                        if (keyInfo.BitSize == null)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, "Signature has an unknown DSA key size.");
+                            result = RuleResult.Fail;
+                        }
+                        else if (keyInfo.BitSize > MAX_DSA_KEY_SIZE)
+                        {
+                            verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses DSA signature with a key size of {keyInfo.BitSize} exeeding maximum size of {MAX_DSA_KEY_SIZE}.");
+                            result = RuleResult.Fail;
+                        }
+                        break;
+                    case PublicKeyAlgorithm.Other:
+                        goto default;
+                    default:
+                        verboseWriter.LogSignatureMessage(signature.SignerInfo, $"Signature uses an unknown algorithm.");
+                        result = RuleResult.Fail;
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
